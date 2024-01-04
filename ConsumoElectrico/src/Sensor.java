d97275eeda53bb2bc22c5431fe813805e5605389
Viviana  Suquilanda <148469465+ViViana2710@users.noreply.github.com>
2024-01-04 17:11:33 -0500

    Update Sensor.java

--- ConsumoElectrico/src/Sensor.java
+++ ConsumoElectrico/src/Sensor.java
@@ -1,13 +1,43 @@
 public class Sensor {
+    //Atributos
     private String numero;
     protected boolean estado;
     private String ubicacion;
     protected double lecturaActual;
     protected double lecturaAnterior;
+    //Metodos
     public void leerDatos() {
+        System.out.println("Leyendo datos del sensor " + numero);
     }
 
     public int getDatos() {
-        return 0;
+        return lecturaActual;
     }
+    //Constructor
+    public Sensor(String numero, boolean estado, String ubicacion) {
+        this.numero = numero;
+        this.estado = estado;
+        this.ubicacion = ubicacion;
+    }
+    //Metodos get
+    public String getNumero() {
+        return numero;
+    }
+
+    public boolean getEstado() {
+        return estado;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public double getLecturaActual() {
+        return lecturaActual;
+    }
+
+    public double getLecturaAnterior() {
+        return lecturaAnterior;
+    }
+
 }
