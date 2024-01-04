ee2c21fab90ead5111f63d636370226fe1056707
Viviana  Suquilanda <148469465+ViViana2710@users.noreply.github.com>
2024-01-04 16:43:21 -0500

    Update Edificio.java

--- ConsumoElectrico/src/Edificio.java
+++ ConsumoElectrico/src/Edificio.java
@@ -2,20 +2,28 @@ public class Edificio {
     private int numeroPisos;
 
     public static class Piso{
-        public int numeroPiso; //diagrama
+        public int numeroPiso; 
         private Edificio edificio;
 
+        public Piso(Edificio edificio, int numeroPiso) {
+            this.edificio = edificio;
+            this.numeroPiso = numeroPiso;
+        }
+
+        private List<Piso> pisos = new ArrayList<>();
+        
         public double CalcularConsumoPiso() {
             return 0;
         }
 
-        public void crearPiso(int i) { //diagrama
-            this.numeroPiso = i;
+        public void crearPiso(int numeroPiso) {
+            Piso nuevoPiso = new Piso(this, numeroPiso);
+            pisos.add(nuevoPiso);
         }
+        
     }
     public double calcularConsumoTotal() {
         double consumo = 0;
-        Piso[] pisos = new Piso[0];
         for (Piso piso : pisos) {
             consumo += piso.CalcularConsumoPiso();
         }
