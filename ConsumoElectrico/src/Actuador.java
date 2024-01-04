ac358c2bd8bcf56155d946ca5285d0d57f1c0547
Viviana  Suquilanda <148469465+ViViana2710@users.noreply.github.com>
2024-01-04 17:15:54 -0500

    Update Actuador.java

--- ConsumoElectrico/src/Actuador.java
+++ ConsumoElectrico/src/Actuador.java
@@ -1,11 +1,11 @@
 public class Actuador {
+    //Atributo
     public double potencia;
     private String actuador;
     protected boolean estado;
     private String ubicacion;
     private String datosSensor;
-
-
+    //Metodos
     public void cambiarEstado() {
          estado = !estado;
     }
