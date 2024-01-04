4364777d58ef7792ad5022ef486695dc3c5e189e
Viviana  Suquilanda <148469465+ViViana2710@users.noreply.github.com>
2024-01-04 17:51:06 -0500

    Update Usuario.java

--- ConsumoElectrico/src/Usuario.java
+++ ConsumoElectrico/src/Usuario.java
@@ -1,9 +1,10 @@
 public class Usuario {
+    //Atributos
     String nombre;
     String apellido;
     String correo;
     String contrasena;
-
+    //Metodos
     public void crearUsuario(String nombre, String apellido, String correo, String contrasena) {
         this.nombre = nombre;
         this.apellido = apellido;
@@ -19,19 +20,36 @@ public class Usuario {
         }
     }
     public boolean cerrarSesion() {
+         System.out.println("Sesión cerrada.");
         return true;
     }
 
-    public void modificarContrasena(String contrasena) {
-        this.contrasena = contrasena;
+    public void modificarContrasena(String nuevaContrasena) {
+        this.contrasena = nuevaContrasena;
+        System.out.println("Contraseña modificada.");
     }
 
     public boolean eliminarUsuario() {
-
+        System.out.println("Usuario eliminado.");
         return true;
     }
 
     public boolean verificarUsuario() {
+        System.out.println("Usuario verificado.");
         return true;
     }
+
+        // Métodos getter para obtener información del usuario
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
 }
