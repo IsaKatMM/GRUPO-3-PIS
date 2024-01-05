8a6fbc86ba3e4e7ba91fc8382e9502b0f80a8f99
Isabel Katherine Morocho Marizaca <148294796+IsaKatMM@users.noreply.github.com>
2024-01-05 00:38:15 -0500

    Update Edificio.java

--- ConsumoElectrico/src/Edificio.java
+++ ConsumoElectrico/src/Edificio.java
@@ -5,12 +5,12 @@ public class Edificio {
     public static class Piso{
         public int numeroPiso; 
         private Edificio edificio;
-
+     //constructor- da valores a atributos
         public Piso(Edificio edificio, int numeroPiso) {
             this.edificio = edificio;
             this.numeroPiso = numeroPiso;
         }
-
+       //pisos se almacen en una lista
         private List<Piso> pisos = new ArrayList<>();
         
         public double CalcularConsumoPiso() {
@@ -25,6 +25,7 @@ public class Edificio {
     }
 
     //Metodo
+    //suma el consumo de cada piso 
     public double calcularConsumoTotal() {
         double consumo = 0;
         for (Piso piso : pisos) {
