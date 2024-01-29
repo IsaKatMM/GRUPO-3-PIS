1d600d78037b7183a0689e21de9360227f9ed116
Cristian Correa <cristian.correa@unl.edu.ec>
2024-01-29 08:35:47 -0500

    Settings

--- task/views.py
+++ task/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'C:/Users/crisc/Documents/GitHub/GRUPO-3-PIS/index.html')
+    return render(request, 'index.html')
