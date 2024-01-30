1d9fd3f50e90e73d0b56ca2ebeb74a5aef197506
Cristian Correa <cristian.correa@unl.edu.ec>
2024-01-30 16:06:41 -0500

    Deletes

--- djangocrud/Templates/js/edificio.js
+++
@@ -1,8 +0,0 @@
-function calcularConsumoTotal() {
-    var calcularConsumoTotal1 = parseFloat(document.getElementById("calcularConsumoTotal1").value) || 0;
-    var calcularConsumoTotal2 = parseFloat(document.getElementById("calcularConsumoTotal2").value) || 0;
-    var calcularConsumoTotal3 = parseFloat(document.getElementById("calcularConsumoTotal3").value) || 0;
-
-    var calcularConsumoTotal = calcularConsumoTotal1 + calcularConsumoTotal2 + calcularConsumoTotal3;
-    alert("El consumo total es de " + calcularConsumoTotal + " amperios.");
-  }
