4477fe916c1eff77136e32ce31941ecb1c00c205
Cristian Correa <cristian.correa@unl.edu.ec>
2024-02-01 11:14:25 -0500

    Django

--- tasks/views.py
+++ tasks/views.py
@@ -31,9 +31,9 @@ def registro(request):
     if request.method == 'GET':
         return render(request, 'registro.html')
     else:
-        if request.POST['password1'] == request.POST['password2']:
+        if request.POST['password'] == request.POST['password']:
             try:
-                user = User.objects.create_user(email=request.POST['email'], password=request.POST['password1'])
+                user = User.objects.create_user(email=request.POST['email'], password=request.POST['password'])
                 user.save()
                 return HttpResponse('Usuario creado sastifactoriamente')
                 
