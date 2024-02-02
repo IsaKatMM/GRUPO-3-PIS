86476c69f290d0d6a3296e89264212f32b564461
Cristian Correa <cristian.correa@unl.edu.ec>
2024-02-01 21:36:41 -0500

    Login

--- tasks/views.py
+++ tasks/views.py
@@ -6,17 +6,23 @@ from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'index.html')
+
 
 def nosotros(request):
     return render(request, 'Nosotros.html')
 
+
 def servicio(request):
     return render(request, 'Servicio.html')
 
+
 def contacto(request):
     return render(request, 'contacto.html')
+
 
 def iniciarSesion(request):
     if request.method == 'GET':
@@ -26,23 +32,27 @@ def iniciarSesion(request):
         print('Obteniendo formulario')
     return render(request, 'iniciarSesion.html')
 
+
 def registro(request):
     if request.method == 'GET':
         return render(request, 'registro.html')
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
-                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
                 user.save()
                 return HttpResponse('Usuario creado sastifactoriamente')
-                
+
             except:
-                return HttpResponse('El usuario ya existe')
-            
-        return HttpResponse('Las contrasenas no coinciden')
-        
+                return render(request, 'registro.html', {
+                    HttpResponse('El usuario ya existe')
+                })
+
+        return render(request, 'registro.html', {
+            "error": 'Las contrasenas no coinciden'
+        })
+
+
 def sistema(request):
     return render(request, 'Sistema.html')
-
-
-
