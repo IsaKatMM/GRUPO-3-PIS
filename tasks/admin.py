96d9803c5a1c3b3d18cff730f5516745b507a6f1
Cristian Correa <cristian.correa@unl.edu.ec>
2024-02-19 16:04:54 -0500

    commits

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Task
+from .models import Edificio
 
-class TaskAdmin(admin.ModelAdmin):
-    readonly_fields = ("created", )
+#class TaskAdmin(admin.ModelAdmin):
+    #readonly_fields = ("created", )
 
 # Register your models here.
-admin.site.register(Task, TaskAdmin)
+#admin.site.register(Task, TaskAdmin)
+
+
+# Register your models here.
+admin.site.register(Edificio,)
--- tasks/models.py
+++ tasks/models.py
@@ -2,7 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Task(models.Model):
+
+class Edificio(models.Model):
+    nombre = models.CharField(max_length=100) 
+    numeroPisos = models.IntegerField(input) 
+    ubicacion = models.CharField(max_length=100) 
+    date = models.DateTimeField(auto_now_add=True) 
+    description = models.TextField(blank=True) 
+    important = models.BooleanField(default=False) 
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+
+#class Task(models.Model):
     tittle = models.CharField(max_length=100) 
     description = models.TextField(blank=True) 
     created = models.DateTimeField(auto_now_add=True) 
@@ -12,3 +22,4 @@ class Task(models.Model):
     
     def __str__(self) :
         return self.tittle + '- by ' + self.user.username
+    
