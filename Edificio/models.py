2902b0f39f31835fa75c9ff575ff07068867878b
Cristian Correa <cristian.correa@unl.edu.ec>
2024-02-23 11:53:11 -0500

    Edificio

--- Edificio/models.py
+++ Edificio/models.py
@@ -34,7 +34,7 @@ class Piso(models.Model):
 #    edificio = models.ForeignKey(Edificio, on_delete=models.CASCADE, default=None) 
     numero_piso = models.IntegerField()
     contador_piso = models.IntegerField()
-    edificio_id = models.ForeignKey(Edificio, on_delete=models.CASCADE, default=None)
+    edificio = models.ForeignKey(Edificio, on_delete=models.CASCADE, default=Edificio)
 
     def __str__(self):
-        return f"Piso {self.numero_piso} del edificio {self.edificio_id.nombre}"
+        return f"Piso {self.numero_piso} del edificio {self.edificio.nombre}"
