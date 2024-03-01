45caf53b65e577a8a1c0229b902750da36579c70
Ushiwakamaru <dany.cabrera@unl.edu.ec>
2024-03-01 09:11:56 -0500

    Energia

--- Repetix/urls.py
+++ Repetix/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from Repetix import views
+from Repetix.views import GenerarEnergiaEolica
 
 urlpatterns = [
     path('', views.index, name='Index'),
@@ -7,17 +8,21 @@ urlpatterns = [
     path('Nosotros/', views.nosotros, name='Nosotros'),
     path('Servicio/', views.servicio, name='Servicio'),
     path('contacto/', views.contacto, name='Contacto'),
-    path('iniciarSesion/', views.iniciarSesion, name='iniciarSesion'),   
+    path('iniciarSesion/', views.iniciarSesion, name='iniciarSesion'),
     path('registro/', views.registro, name='Registro'),
     path('Sistema/', views.sistema, name='Sistema'),
     path('Informe/', views.informe, name='Informe'),
     path('logout/', views.cerrarSesion, name='logout'),
-    path('recuperarContrasena/', views.recuperacion, name='recuperarContrasena'), 
+    path('recuperarContrasena/', views.recuperacion, name='recuperarContrasena'),
     path('registrar_piso/', views.registrar_piso, name='registrar_piso'),
     path('RegistrarEdificio2/', views.registrarEdificio, name='RegistrarEdificio2'),
     path('AccederEdificio/', views.accederEdificio, name='AccederEdificio'),
-    path('control/<codigo>', views.control, name='ControlarEdificio'),    
+    path('control/<codigo>', views.control, name='ControlarEdificio'),
     path('ControlarEdificio/', views.ControlarEdificio, name='ControlarEdificio'),
     path('EliminarEdificio/<codigo>', views.eliminacion, name='EliminarEdificio'),
     path('actualizar_tabla_pisos/', views.actualizar_tabla_pisos, name='actualizar_tabla_pisos'),
+    path('energia_eolica/', views.energia_eolica, name='energia_eolica'),
+    path('generar_energia_eolica/',views.GenerarEnergiaEolica, name='generar_energia_eolica'),
+
 ]
+
